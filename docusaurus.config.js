a4230d7b811ec11b320ac337908cccf91304afc5
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2023-05-23 16:36:37 +0500

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,106 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "zkBNB -  Official Documentation",
+  tagline: "A trustless and scaling solution for BNB Smart Chain",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "RumeelHussainbnb", // Usually your GitHub org/user name.
+  projectName: "zkBNB-docs", // Usually your repo name.
+  trailingSlash: false,
+
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          //editUrl:
+            //"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          docLayoutComponent: "@theme/DocPage",
+          docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: true
+        }
+      },
+      navbar: {
+        logo: {
+          alt: "zkBNB Logo",
+          src: "img/logo.svg"
+        },
+        items: [
+          {
+            label: "API Docs",
+            position: "left",
+            to: "/docs/category/zkbnb-api"
+          },
+        ]
+      },
+      
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["ruby", "csharp", "php",]
+      }
+    }),
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          zkbnb: {
+            specPath: "swagger/zkbnb.yaml",
+            outputDir: "docs/zkbnb",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+            sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag"
+            },
+          }
+        }
+      }
+    ]
+  ],
+
+  themes: ["docusaurus-theme-openapi-docs"]
+};
+
+module.exports = config;
