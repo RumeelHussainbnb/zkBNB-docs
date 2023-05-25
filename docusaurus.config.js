61805a109274d881dfc6288b0fe90f502cb95647
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2023-05-25 14:16:50 +0500

    updated config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "RumeelHussainbnb", // Usually your GitHub org/user name.
-  projectName: "zkBNB-docs", // Usually your repo name. 
+  projectName: "zkbnb-docs", // Usually your repo name. 
   deploymentBranch: 'gh-pages',
   //trailingSlash: false,
 
